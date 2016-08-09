689aef32f24a36a051e4497e2bc8190f18404d26
Jonathan Heathcote <mail@jhnet.co.uk>
2016-08-09 18:06:30 +0100

    Add station database parser.

---
+++ railmap/stations.py
@@ -0,0 +1,32 @@
+"""Parse the station information file.
+"""
+
+import dbf
+
+from collections import namedtuple
+
+
+StationRecord = namedtuple("StationRecord", "name,station_code,eastings,northings")
+
+
+def dbf_to_dict(filename):
+    """Read a DBF file containing station information from Network Rail's
+    Railway Network Inspire data. Produces a dict which maps from station
+    three-letter-code to its name and location.
+    """
+    t = dbf.Table(filename)
+    
+    stations = {}
+    
+    t.open()
+    try:
+        for row in t:
+            stations[row.stn_code.strip()] = StationRecord(
+                row.name.strip(),
+                row.stn_code.strip(),
+                row.gis_eastin,
+                row.gis_northi)
+    finally:
+        t.close()
+    
+    return stations
